bec74583a806dfed7746e37f69c763f78f7c1caa
umutcirak <umut.cirak@ozu.edu.tr>
2022-03-27 19:30:57 +0300

    Enemy Script is built.

---
+++ Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] float enemySpeed = 1f;
+
+    Rigidbody2D enemyRigidBody;
+    CapsuleCollider2D enemyBodyCollider;
+
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+        enemyBodyCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    void Update()
+    {
+        enemyRigidBody.velocity = new Vector2(enemySpeed, 0f);
+
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        enemySpeed = -enemySpeed;
+        FlipEnemy();
+    }
+
+
+    void FlipEnemy()
+    {
+        transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+    }
+
+
+
+}
